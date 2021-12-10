4c95b56b02c6e13c5b7735c2f7268aa9faac942e
devtplex <63226790+hasan-rafay-tplex@users.noreply.github.com>
2021-12-10 21:20:53 +0500

    Office Work

---
+++ ProjectEvalutionSystem/Controllers/EvalutionIndexController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ProjectEvalutionSystem.Models;
+using ProjectEvalutionSystem.Models.EvalutionIndexDTOs;
+
+namespace ProjectEvalutionSystem.Controllers
+{
+    public class EvalutionIndexController : Controller
+    {
+        // GET: Evalution
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetAll()
+        {
+            try
+            {
+                using (ProjectEvalutionSystemEntities _context = new ProjectEvalutionSystemEntities())
+                {
+                    var result = await _context.EvalutionIndexes
+                        .Include(x=> x.Student)
+                        .Include(x => x.Teacher)
+                        .Include(x => x.Assignment)
+                        .ToListAsync();
+                    if (result.Any())
+                    {
+                        var ConvertedDTO = result.ConvertAll(EvalutionIndexDTO.EvalutionIndexConverter);
+                        return Json(ConvertedDTO,JsonRequestBehavior.AllowGet);
+                    }
+
+                    return Json(null, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ ProjectEvalutionSystem/Models/EvalutionIndexDTOs/EvalutionIndexDTO.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEvalutionSystem.Models.EvalutionIndexDTOs
+{
+    public class EvalutionIndexDTO
+    {
+        public int Id { get; set; }
+        public DateTime? EvalutionDate { get; set; }
+        public string Remarks { get; set; }
+        public string Comments { get; set; }
+        public int? StudentID { get; set; }
+        public int? TeacherID { get; set; }
+        public int? AssignmentID { get; set; }
+
+        public string AssignmentName { get; set; }
+        public string StudentName { get; set; }
+        public string TeacherName { get; set; }
+
+        public static EvalutionIndexDTO EvalutionIndexConverter(EvalutionIndex input)
+        {
+            return new EvalutionIndexDTO()
+            {
+                Id = input.ID,
+                EvalutionDate = input.EvalutionDate,
+                Remarks = input.Remarks,
+                Comments = input.Comments,
+                StudentID = input.StudentID,
+                TeacherID = input.TeacherID,
+                AssignmentID = input.AssignmentID,
+                AssignmentName = input.Assignment.Name,
+                StudentName = input.Student.FullName,
+                TeacherName = input.Teacher.FullName
+            };
+        }
+    }
+
+    
+}
