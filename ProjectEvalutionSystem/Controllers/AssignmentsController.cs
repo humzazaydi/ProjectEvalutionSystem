8868b9385d4408dd9983d75cde42a58f3d925e76
Syed Hamza Zaidi <hamza.zaidi@technologyplex.com>
2022-01-04 20:05:36 +0500

    assignment upload functionality has been done

--- ProjectEvalutionSystem/Controllers/AssignmentsController.cs
+++ ProjectEvalutionSystem/Controllers/AssignmentsController.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.Mvc;
 using ProjectEvalutionSystem.Models;
+using ProjectEvalutionSystem.Models.AssignmentDTOs;
 
 namespace ProjectEvalutionSystem.Controllers
 {
@@ -13,7 +16,10 @@ namespace ProjectEvalutionSystem.Controllers
     {
         private ProjectEvalutionSystemEntities db = new ProjectEvalutionSystemEntities();
         // GET: Assignments
-       
+        public ActionResult Index()
+        {
+            return View();
+        }
         [HttpGet]
         public async Task<JsonResult> GetAsync()
         {
@@ -27,7 +33,7 @@ namespace ProjectEvalutionSystem.Controllers
                 return Json(exception.InnerException, JsonRequestBehavior.AllowGet);
             }
         }
-        
+
         [HttpGet]
         [Route("GetAsync/{assignmentId}")]
         public async Task<JsonResult> GetAsync(int assignmentId)
@@ -42,26 +48,61 @@ namespace ProjectEvalutionSystem.Controllers
                 return Json(exception.InnerException, JsonRequestBehavior.AllowGet);
             }
         }
-        
+
         [HttpPost]
-        public async Task<JsonResult> PostAsync(Assignment input)
+        public async Task<JsonResult> PostAsync([Form] AssignmentsDTO input)
         {
             try
             {
-                input.CreationTimeStamp = DateTime.Now;
-                db.Assignments.Add(input);
-                await db.SaveChangesAsync();
+                if (input.Files != null)
+                {
+                    //Get all files from Request object  
+                    HttpPostedFileBase file = input.Files;
+                    string fname;
 
-                return Json(input.ID, JsonRequestBehavior.AllowGet);
+                    // Checking for Internet Explorer  
+                    if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                    {
+                        string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                        fname = testfiles[testfiles.Length - 1];
+                    }
+                    else
+                    {
+                        fname = file.FileName;
+                    }
+
+                    // Get the complete folder path and store the file inside it.  
+                    fname = Path.Combine(Server.MapPath("~/App_Data/"), fname);
+                    file.SaveAs(fname);
+                    var assignmentTobeAdd = new Assignment
+                    {
+                        TeacherStudent = input.TeacherStudentID,
+                        Name = input.Name,
+                        Description = input.Description,
+                        Path = file.FileName,
+                        IsDeleted = false,
+                        CreationTimeStamp = DateTime.Now
+                    };
+
+                    db.Assignments.Add(assignmentTobeAdd);
+                    await db.SaveChangesAsync();
+
+                    return Json(input.ID, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json("Please upload a document", JsonRequestBehavior.AllowGet);
+                }
+                
             }
             catch (Exception exception)
             {
                 return Json(exception.InnerException, JsonRequestBehavior.AllowGet);
             }
         }
-       
+
         [HttpPut]
-        public async Task<JsonResult> PutAsync(Assignment input)
+        public async Task<JsonResult> PutAsync(AssignmentsDTO input)
         {
             try
             {
@@ -70,7 +111,7 @@ namespace ProjectEvalutionSystem.Controllers
                 {
                     assignment.Name = input.Name;
                     assignment.Description = input.Description;
-                    assignment.TeacherStudent = input.TeacherStudent;
+                    assignment.TeacherStudent = input.TeacherStudentID;
 
                     db.Entry(assignment).State = EntityState.Modified;
                     await db.SaveChangesAsync();
---
+++ ProjectEvalutionSystem/Models/AssignmentDTOs/AssignmentsDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectEvalutionSystem.Models.AssignmentDTOs
+{
+    public class AssignmentsDTO
+    {
+        public int ID { get; set; }
+        public int? TeacherStudentID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+        public HttpPostedFileBase Files { get; set; }
+    }
+}
